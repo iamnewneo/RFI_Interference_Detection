30ac67bd3c983401b9fcdf7f100ea61f278ca743
Kunal Gusain <kunalgusain@Kunals-MacBook-Pro.local>
2021-01-24 15:24:41 -0500

    Model Fixes

--- rfi_class/model/model.py
+++ rfi_class/model/model.py
@@ -3,6 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 from rfi_class import config
 import pytorch_lightning as pl
+from torchvision import models
 
 
 class RFIModel(pl.LightningModule):
@@ -11,27 +12,46 @@ class RFIModel(pl.LightningModule):
         self.n_classes = len(config.CLASSES)
         pool_size = (2, 2)
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
-        self.maxpool = nn.AdaptiveMaxPool2d(pool_size)
-        self.fc1 = nn.Linear(128 * pool_size[0] * pool_size[1], 512)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 256, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(256 * pool_size[0] * pool_size[1], 512)
         self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, self.n_classes)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, self.n_classes)
+        self.maxpool = nn.AdaptiveMaxPool2d(pool_size)
+        self.dropout = nn.Dropout(p=0.5)
         self.accuracy = pl.metrics.Accuracy()
         self.train_accuracy = pl.metrics.Accuracy()
         self.valid_accuracy = pl.metrics.Accuracy()
 
     def forward(self, x):
-        out = F.relu(self.conv1(x))
-        out = F.relu(self.conv2(out))
-        out = F.relu(self.conv3(out))
-        out = F.relu(self.conv4(out))
-        out = self.maxpool(out)
+        # C > M > R > B
+        out = F.relu(self.maxpool(self.conv1(x)))
+        out = self.bn1(out)
+        out = F.relu(self.maxpool(self.conv2(out)))
+        out = self.bn2(out)
+        out = F.relu(self.maxpool(self.conv3(out)))
+        out = self.bn3(out)
+        out = F.relu(self.maxpool(self.conv4(out)))
+        out = self.bn4(out)
+        out = F.relu(self.maxpool(self.conv5(out)))
+        out = self.bn5(out)
         out = out.view(out.size(0), -1)
-        out = self.fc1(out)
-        out = self.fc2(out)
-        out = self.fc3(out)
+        out = F.relu(self.fc1(out))
+        out = self.dropout(out)
+        out = F.relu(self.fc2(out))
+        out = self.dropout(out)
+        out = F.relu(self.fc3(out))
+        out = self.dropout(out)
+        out = F.relu(self.fc4(out))
+        out = F.log_softmax(out)
         return out
 
     def loss_fn(self, out, target):
